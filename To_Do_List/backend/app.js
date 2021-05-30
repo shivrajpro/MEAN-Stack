6c98f3080079df5aac9f9b7277f98e360edbbb9b
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2021-05-30 23:55:41 +0530

    Todo-List: handling CORS

--- To_Do_List/backend/app.js
+++ To_Do_List/backend/app.js
@@ -3,10 +3,23 @@ const app = express();
 
 const mongoose = require('./database/mongoose');
 
-app.listen(8000, ()=>{
+/* 
+CORS: Cross Origin Request Security
+    localhost:8000 - backend API
+    localhost:4200 - frontend
+ */
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,HEAD,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.json());
+app.listen(8000, () => {
     console.log("Server Connected on Port 8000");
 })
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello");
 })
--- To_Do_List/backend/database/mongoose.js
+++ To_Do_List/backend/database/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect("mongodb://localhost:27017/todo_list")
+mongoose.connect("mongodb://localhost:27017/todo_list", {useNewUrlParser: true, useUnifiedTopology:true})
     .then(() => console.log("Database Connection Successful!"))
     .catch(e => console.log("Database Connection Failed!", e));
 
